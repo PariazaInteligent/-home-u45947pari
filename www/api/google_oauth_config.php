cc5dc728d0ad545bd6fabc203aa0e28d05f9cac7
PariazaInteligent <tomizeimihaita@gmail.com>
2025-12-05 17:12:54 +0100

    up

--- www/api/google_oauth_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-  'client_id'     => '205158128640-79bo86bqu23j1sgsnt67ms4l80886nqi.apps.googleusercontent.com',
-  'client_secret' => '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',
-  'redirect_uri'  => 'https://pariazainteligent.ro/auth/google/callback.php',
-  // 'allowed_hd'  => 'domeniu.ro', // opțional, dacă vrei doar un domeniu GSuite
-];
